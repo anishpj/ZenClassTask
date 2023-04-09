50bd71db76a75e663f95e8d0706f9690ec53d678
Anish PJ <ljcanish@gmail.com>
2023-04-09 11:52:46 +0530

    Old_Commit
    Old_Commit

---
+++ Mern-Day6/Day6Sample.js
@@ -0,0 +1,44 @@
+// Part1: Q2
+alert("I'm invoked from js file for Part 1 Question no. 2");
+
+// Part1: Q3
+alert("I'm JavaScript! for Part 1 Question no. 3");
+alert('Hello') // this line is not having semicolon
+alert(`Wor
+ ld`) 
+alert(3 +
+1
++ 2); // this is multiple line code and its working
+
+// Part1: Q4
+let admin=9, fname=10.5; 
+fname = '"Guvi';
+lname = ' geek"';
+admin = fname+lname;
+alert(admin + " for Part 1 Question no. 4"); // "Guvi geek"
+
+// Part1: Q5
+let fname1=10.5; 
+fname1 = "Guvi";
+lname1 = "geek"
+let name1 = fname1+lname1;
+alert( 'hello ${name}' );
+
+
+
+
+
+
+
+
+
+// Part2: 
+// Write a code to print the numbers in the array
+// Output: 1234567891011
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var new_string = "";
+for (var i = 0; i < numsArr.length; i++) {
+ new_string += numsArr[i] 
+}
+console.log(new_string);
+
